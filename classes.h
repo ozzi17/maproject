e558d8308c280fe5170f03731fa59c10209adf88
MeloFinalBoss <guilhermehsdm@gmail.com>
2025-07-03 10:18:41 -0300

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,19 +1,41 @@
+#include <fstream>
+#include <iostream>
+
 class Cor{
     
-    public:
-    int R, G, B;
-};
+  public:   
+  int R;
+  int G; 
+  int B;
 
-class Paleta{
+};    
+
+ class Paleta{
+
     int quantidade;
-    Cor cores[100];
+    Cor cores[100]; 
     int valor[100]; 
-    public:
-    
-};
+   
+     public: 
 
-class Imagem{
-    int larg, alt;
-    
+     void Leitura(std::string nomeArquivo){
+        
+     std:: ifstream arquivo(nomeArquivo);
+  
+     if(!arquivo.is_open()){
+       std::cout << "Erro ao abrir o arquivo!" << std::endl;
+     }
+  
+     int N;
 
-};
+     arquivo >> N;
+
+     for(int i = 0; i < N; ++i){
+      arquivo >> valor[i]>> cores[i].R >> cores[i].G >> cores[i].B;
+     }
+
+      arquivo.close();
+     }
+};  
+
+
