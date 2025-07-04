d3aec0f29977f5eb3d6d34ee979fc77c60c475b4
ozzi17 <oslifernandes@gmail.com>
2025-07-04 10:49:23 -0300

    Implement Leitura and ConsultaCor methods in Paleta class

---
+++ paleta.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include "paleta.h"
+void Paleta::Leitura(std::string nomeArquivo)
+    {
+        
+     std:: ifstream arquivo(nomeArquivo);
+  
+     if(!arquivo.is_open())
+     {
+       std::cout << "Erro ao abrir o arquivo!" << std::endl;
+     }
+
+     arquivo >> quantidade;
+
+     for(int i = 0; i < quantidade; ++i)
+     {
+      arquivo >> valor[i]>> cores[i].R >> cores[i].G >> cores[i].B;
+     }
+
+      arquivo.close();
+    }
+
+     Cor Paleta::ConsultaCor(double value)
+    {
+      if (quantidade == 0)
+      {
+        return {0, 0, 0};
+      }
+      
+      for (int i = 0; i < quantidade - 1; i++)
+      {
+        if (value >= valor[i] && value < valor[i+1])
+        {
+          return cores[i];
+        }
+      }
+      return cores[quantidade - 1];
+    }
--- paleta.h
+++ paleta.h
@@ -21,42 +21,9 @@ class Cor{
    
      public: 
 
-     void Leitura(std::string nomeArquivo)
-     {
-        
-     std:: ifstream arquivo(nomeArquivo);
-  
-     if(!arquivo.is_open())
-     {
-       std::cout << "Erro ao abrir o arquivo!" << std::endl;
-     }
+     void Leitura(std::string nomeArquivo);
 
-     arquivo >> quantidade;
-
-     for(int i = 0; i < quantidade; ++i)
-     {
-      arquivo >> valor[i]>> cores[i].R >> cores[i].G >> cores[i].B;
-     }
-
-      arquivo.close();
-     }
-
-     Cor ConsultaCor(double value)
-     {
-      if (quantidade == 0)
-      {
-        return {0, 0, 0};
-      }
-      
-      for (int i = 0; i < quantidade - 1; i++)
-      {
-        if (value >= valor[i] && value < valor[i+1])
-        {
-          return cores[i];
-        }
-      }
-      return cores[quantidade - 1];
-     }
+     Cor ConsultaCor(double value);
 };
 
 #endif
