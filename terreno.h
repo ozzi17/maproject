0e17d12444f500b350047f6e36ba9a1367b3f4a5
MeloFinalBoss <guilhermehsdm@gmail.com>
2025-07-10 10:31:28 -0300

    Create terreno.h

---
+++ terreno.h
@@ -0,0 +1,48 @@
+#ifndef TERRENO_H
+#define TERRENO_H
+
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+class MapaAltitudes {
+private:
+    int N;
+    int tamanho;
+    float rugosidade;
+    int** matriz;
+  
+    public: 
+    // Função de inicialização do mapa
+    void gerarMapa();
+
+    // Construtor da classe
+    MapaAltitudes(int N, float rugosidade)
+        : N(N), rugosidade(rugosidade) {
+        tamanho = (1 << N) + 1;  // 2^N + 1
+        matriz = new int*[tamanho];
+        for (int i = 0; i < tamanho; ++i) {
+            matriz[i] = new int[tamanho]();
+        }
+
+        srand(time(0));  // Inicializar o gerador de números aleatórios
+        gerarMapa();
+    }
+
+    // Destruidor da classe
+    ~MapaAltitudes() {
+        for (int i = 0; i < tamanho; ++i) {
+            delete[] matriz[i];
+        }
+        delete[] matriz;
+    }
+
+};
+
+
+
+
+
+
+#endif
