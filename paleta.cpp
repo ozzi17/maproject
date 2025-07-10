d7d47ad800fa25ad4f28c450031a17f06418d3ae
MeloFinalBoss <guilhermehsdm@gmail.com>
2025-07-10 09:25:52 -0300

    Update paleta.cpp

--- paleta.cpp
+++ paleta.cpp
@@ -38,34 +38,4 @@ void Paleta::Leitura(std::string nomeArquivo)
       return cores[quantidade - 1];
     }
 
-    Cor Imagem::consultaPixel(int largura, int altura)
-    {
-      return pixels[largura][altura];
-    }
-
-    void Imagem::defineCor(int largura, int altura, int r, int g, int b)
-    {
-      pixels[largura][altura].R = r;
-      pixels[largura][altura].G = g;
-      pixels[largura][altura].B = b;
-    }
-
-    void Imagem::salvaPPM()
-    {
-      std::ofstream outputFile("teste.ppm");
-      if (outputFile.is_open())
-      {
-        outputFile << "P3" << std::endl;
-        outputFile << "100 100" << std::endl;
-        outputFile << "255" << std::endl;
-        for (int i = 0; i < 100; i++)
-        {
-          for (int j = 0; j < 100; j++)
-          {
-            outputFile << pixels[i][j].R << " " << pixels[i][j].G << " " << pixels[i][j].B << std::endl;
-          }
-          
-        }
-        
-      }
-    }
+    
